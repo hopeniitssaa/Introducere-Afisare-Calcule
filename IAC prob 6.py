a55f5993d02781e937d3858d86bfc293bc76e0ec
Băbuță Luminița <70962140+hopeniitssaa@users.noreply.github.com>
2020-10-06 21:58:46 +0300

    Add files via upload

---
+++ IAC prob 6.py
@@ -0,0 +1,8 @@
+"""Doi copii au primit acelaşi număr de mere Introducând de la tastatură numărul de mere
+primte, afişaţi câte mere are fiecare copil după ce primul copil mănâncă un măr şi dă
+unul celuilalt copil. Exemplu : Date de intrare : 10 Date de ieşire : primul copil 8
+mere al doilea copil 11 mere. """
+
+n=int(input("dati numarul de mere primite de copii"))
+print("primul copil are : ",n-2, "mere")
+print("al doilea copil are :",n+1,"mere")
