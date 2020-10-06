811925c1ab63c0e85b9a40d6623e73bddec641bb
Băbuță Luminița <70962140+hopeniitssaa@users.noreply.github.com>
2020-10-06 22:11:42 +0300

    Add files via upload

---
+++ IAC prob 10.py
@@ -0,0 +1,14 @@
+#Afişaţi tabla înmulţirii cu numărul n. Exemplu: pentru n=5, se va afişa pe verticală
+#1x5=5 2x5=10 3x5=15 4x5=20 5x5=25 6x5=30 7x5=35 8x5=40 9x5=45 10x5=50.
+
+n = int(input("dati un numar: "))
+print(n, "x", 1, "=", n*1) 
+print(n, "x", 2, "=", n*2)
+print(n, "x", 3, "=", n*3) 
+print(n, "x", 4, "=", n*4)
+print(n, "x", 5, "=", n*5) 
+print(n, "x", 6, "=", n*6)
+print(n, "x", 7, "=", n*7) 
+print(n, "x", 8, "=", n*8)
+print(n, "x", 9, "=", n*9) 
+print(n, "x", 10, "=", n*10)
