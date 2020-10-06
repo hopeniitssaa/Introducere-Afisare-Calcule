b155a05df2a6385a7ec3325bc56af70d26a145b9
Băbuță Luminița <70962140+hopeniitssaa@users.noreply.github.com>
2020-10-06 21:47:54 +0300

    Add files via upload

---
+++ IAC prob 3.py
@@ -0,0 +1,8 @@
+#Intr-un autobuz care pleacă în excursie sunt 7 copii. De la încă două şcoli urcă alţi
+#copii, numărul acestora citindu-se de la tastatura. Câţi copii au plecat în excursie?
+#Exemplu: Date de intrare: 15 20 Date de ieşire: 42 copii.
+      
+S1=int(input("dati numarul de copii care urca la statia 1 : "))
+S2=int(input("dati numarul de copii care urca la statia 2 : "))
+nt= 7 + S2 + S2 
+print("In ce excursie au plecat copii",nt)
