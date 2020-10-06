94682002d1bae762bea90c2098a0b7b3571b06a7
Băbuță Luminița <70962140+hopeniitssaa@users.noreply.github.com>
2020-10-06 22:16:51 +0300

    Add files via upload

---
+++ IAC prob 12.py
@@ -0,0 +1,9 @@
+#Într-o gospodărie sunt 4 găini. Introduceţi în calculator prin variabilele a, b, c, d
+#numărul de ouă pe care-l dă fiecare găină într-o zi. Afişaţi câte ouă se obţin într-o
+#săptămână. 
+
+a = int(input("dati nr de oua facute de 1 gaina in fiecare zi: "))
+b = int(input("dati nr de oua facute de 2 gaina in fiecare zi: "))
+c = int(input("dati nr de oua facute de 3 gaina in fiecare zi: "))
+d = int(input("dati nr oua facute de 4 gaina in fiecare zi: "))
+print("Numarul de oua facute de toate gainile impreuna este: ", 7 * (a + b + c + d))
