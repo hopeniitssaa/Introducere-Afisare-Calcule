ed76203415193a413e94647fd16366c8c00e6529
Băbuță Luminița <70962140+hopeniitssaa@users.noreply.github.com>
2020-10-06 21:44:26 +0300

    Add files via upload

---
+++ IAC prob 2.py
@@ -0,0 +1,8 @@
+#Într-o tabără numărul de băieţi este cu 10 mai mare decât cel al fetelor. Dacă se citeşte
+#de la tastatură numărul de fete, să se spună câţi elevi sunt în tabără. Exemplu: date de
+#intrare: 50 date de ieşire: 110.
+
+nf = int(input("dati numarul de fete: "))
+nb = nf + 10
+nt = nb + nf
+print("Numarul total de fete si baieti in tabara este: ",nt)
