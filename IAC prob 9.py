3df8cb2be80477bfe91a7f02ae39b917d337bfee
Băbuță Luminița <70962140+hopeniitssaa@users.noreply.github.com>
2020-10-06 22:07:42 +0300

    Add files via upload

---
+++ IAC prob 9.py
@@ -0,0 +1,10 @@
+"""Date trei numere, să se calculeze toate sumele posibile de câte două numere. Afişarea
+să cuprindă şi termenii sumei, nu numai valoarea ei. Exemplu: Date de intrare : 2 13
+4 Date de ieşire: 2+13 =15 2+4=6 13+4=17. """
+
+a = int(input("dati a= "))
+b = int(input("dati b= "))
+c = int(input("dati c= "))
+print(a, "+" ,b,"=",a + b)
+print(a, "+" ,c,"=",a + c)
+print(c, "+" ,b,"=",c + b)
